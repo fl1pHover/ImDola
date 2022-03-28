4c409d46f20828e1f64244300bed73361149aa99
fl1pHover <bishu.hover@gmail.com>
2022-03-28 14:04:09 +0800

    asd

--- slide.js
+++ slide.js
@@ -4,9 +4,9 @@ var link = document.querySelector('.link');
 // Animation class
 var transition = document.querySelector('.transition');
 
-function clicker(page) {
+function clicker(teemaa) {
 	transition.classList.add('slide');
 	setTimeout(() => {
-		window.location = page;
+		window.location = teemaa;
 	}, 900);
 }
