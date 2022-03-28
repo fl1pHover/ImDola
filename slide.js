3798ec1db6474aab33f76e270aa96e25e2489894
fl1pHover <bishu.hover@gmail.com>
2022-03-28 23:32:24 +0800

    asdf

--- slide.js
+++ slide.js
@@ -3,20 +3,10 @@ var link = document.querySelector('.link');
 
 // Animation class
 var transition = document.querySelector('.transition');
-var cover = document.getElementsByClassName('cover');
 function clicker(teemaa) {
 	transition.classList.add('slide');
 	setTimeout(() => {
 		window.location = teemaa;
 	}, 900);
 	// document.body.classList.remove('js-loading');
-}
-
-while (true) {
-	setTimeout(() => {
-		if (history.length > 2) {
-			cover[0].style.display = 'none';
-			break
-		}
-	}, 1000);
 }
